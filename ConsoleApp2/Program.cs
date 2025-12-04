2efb1dd266b7ff69918a1cf7173b33775aae0190
mvh4@tpu.ru <mvh4@tpu.ru>
2025-12-04 11:54:46 +0700

    feature(summary): добавил уравнение

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!"); 
-    Console.WriteLine("HAY!"); 
+    Console.WriteLine("HAY!");
+int summary = 5 + 8;
+Console.WriteLine(summary);
+Console.ReadLine();
